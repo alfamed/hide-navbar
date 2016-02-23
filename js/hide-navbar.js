f5acb5d33cd3013c8320df4dc6010d7a79bb4e62
Andrey Lopatin <lopatin601@gmail.comgit config --global color.ui truels -lagit config --global user.email lopatin601@gmail.com>
2016-02-23 12:27:32 +0200

    Added navbar functionality

--- js/hide-navbar.js
+++ js/hide-navbar.js
@@ -1,7 +1,72 @@
-;$(function(){
-  // TODO: navigation bar element must be hiddent in idle
-  // REVIEW: code must be readable!
-  //
-  //
-  // 
-});
+;$(function() {
+
+  function mobile_depend(window_width) {
+    return $(window).width()<=parseInt(window_width);
+  }
+    $(window).load(function() {
+      var nHeight=$('.navbar').height();
+      var state=false;
+      var slideDuration=500;
+      var lastScrollTop;
+      var newScrollTop;
+      var timerId;
+      var hide_navbar = false;
+
+      function hideNavbar() {
+        $('.navbar').animate({top: -nHeight + "px"}, {
+          duration: slideDuration,
+          easing: 'linear',
+          start: function() {
+            state=false;
+          },
+          done: function() {
+            state=true;
+            hide_navbar=false;
+          }
+        });
+      };//end hideNavbar function
+      console.log($(window).width()<=768);
+      if (mobile_depend(768)&&(!state)) {
+        alert('hide_navbar');
+        setTimeout(hideNavbar(),1000);//hide navbar as soon as page loads
+      };
+
+      var lastScrollTop = $(window).scrollTop();
+      $(window).scroll(function(){
+        var newScrollTop = $(window).scrollTop();
+
+        clearTimeout(timerId);
+        if(hide_navbar) {
+          timerId=setTimeout(hideNavbar, 1000);
+        }
+
+        if (state) {
+          if (newScrollTop < lastScrollTop) {
+            $('.navbar').animate({top: 0 +'px'}, {
+              duration: slideDuration,
+              easing: 'linear',
+              start: function() {
+                state=false;
+              },
+              done: function() {
+                timerId=setTimeout(hideNavbar, 2000);
+              }
+
+            });//navbar is shown after scroll up
+
+          };
+
+        };
+        if(!state) {
+          if (newScrollTop > lastScrollTop) {
+              clearTimeout(timerId);
+              hideNavbar();
+          };
+        };
+
+        lastScrollTop = newScrollTop;
+      });//end scroll
+
+    });//end load
+
+});//end main function
